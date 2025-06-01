5a837f88c009b3678c1ad44fbd2ce71446ca33ed
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-06-01 21:18:05 +0500

    Added more fields

--- lib/View/add_log.dart
+++ lib/View/add_log.dart
@@ -1,64 +1,199 @@
 import 'package:flutter/material.dart';
 
-class AddLogScreen extends StatefulWidget{
+class AddLogScreen extends StatefulWidget {
   const AddLogScreen({super.key});
 
   @override
   AddLogScreenState createState() => AddLogScreenState();
-
 }
 
-class AddLogScreenState extends State<AddLogScreen>{
+class AddLogScreenState extends State<AddLogScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _detailController = TextEditingController();
+  final _purposeController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _dateController = TextEditingController();
+  final _timeController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _purposeController.dispose();
+    _detailController.dispose();
+    _dateController.dispose();
+    _timeController.dispose();
+    super.dispose();
+  }
+
   Future<void> _validateLog() async {
     if (_formKey.currentState!.validate()) {
-
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          content: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            color: Colors.green.shade300,
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Log saved successfully!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          duration: const Duration(seconds: 4),
+        ),
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Log'),
-      ),
-      body: Padding(padding: const EdgeInsets.all(16.0),
-      child: Form(
+      appBar: AppBar(title: const Text('Add Log'), centerTitle: true),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
           child: Column(
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Detail of Journey',
-                prefixIcon: const Icon(Icons.description),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  labelText: 'Name of Official',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
                 ),
-                floatingLabelBehavior: FloatingLabelBehavior.auto,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the name of official';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a detail of journey';
-                }
-                return null;
-              }
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _validateLog,
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(vertical: 12.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                controller: _detailController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  labelText: 'Detail of Journey',
+                  prefixIcon: Icon(Icons.description),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the detail of journey';
+                  }
+                  return null;
+                },
               ),
-              child: const Text('Save', style: TextStyle(fontSize: 18.0)),
-            ),
-
-
-          ],
-          )
-      ),
-
+              const SizedBox(height: 20.0),
+              TextFormField(
+                controller: _purposeController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  labelText: 'Purpose of Journey',
+                  prefixIcon: Icon(Icons.description),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the purpose of journey';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                controller: _dateController,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  labelText: 'Date of Journey',
+                  prefixIcon: Icon(Icons.date_range),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                ),
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                  );
+                  if (pickedDate != null) {
+                    _dateController.text =
+                        "${pickedDate.toLocal()}".split(' ')[0];
+                  }
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the date of journey';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                controller: _timeController,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  labelText: 'Time of Journey',
+                  prefixIcon: Icon(Icons.access_time_filled_sharp),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                ),
+                onTap: () async {
+                  final TimeOfDay? pickedTime = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                    initialEntryMode: TimePickerEntryMode.input,
+                  );
+                  if (pickedTime != null) {
+                    _timeController.text = pickedTime.format(context);
+                  }
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the time of journey';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: _validateLog,
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: const Text('Save', style: TextStyle(fontSize: 18.0)),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
