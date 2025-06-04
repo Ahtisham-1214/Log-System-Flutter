f6489f05b449f61bc570edb344d9e2bc6017a750
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-06-04 19:54:36 +0500

    Added logs table

--- lib/Model/database_helper.dart
+++ lib/Model/database_helper.dart
@@ -30,7 +30,7 @@ class DatabaseHelper {
     // Open the database (or create it if it doesn't exist)
     return await openDatabase(
       path,
-      version: 1,
+      version: 2,
       onCreate: _onCreate,
       onUpgrade: _onUpgrade, // Handle database upgrades if schema changes
     );
@@ -45,14 +45,43 @@ class DatabaseHelper {
         password TEXT NOT NULL
       )
     ''');
+    await db.execute('''
+      CREATE TABLE logs(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        detail TEXT NOT NULL,
+        purpose TEXT NOT NULL,
+        date TEXT NOT NULL,
+        timeFrom TEXT NOT NULL,
+        timeTo TEXT NOT NULL,
+        initialMeterReading REAL NOT NULL,
+        finalMeterReading REAL NOT NULL,
+        remarks TEXT,
+        kilometersCovered REAL NOT NULL
+      )
+      ''');
   }
 
   // Handle database upgrades (e.g., adding new columns)
   Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
     // Example: If you change your schema in a future version
-    // if (oldVersion < 2) {
-    //   await db.execute("ALTER TABLE users ADD COLUMN email TEXT;");
-    // }
+    if (oldVersion < 2) {
+      await db.execute('''
+      CREATE TABLE logs(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        detail TEXT NOT NULL,
+        purpose TEXT NOT NULL,
+        date TEXT NOT NULL,
+        timeFrom TEXT NOT NULL,
+        timeTo TEXT NOT NULL,
+        initialMeterReading REAL NOT NULL,
+        finalMeterReading REAL NOT NULL,
+        remarks TEXT,
+        kilometersCovered REAL NOT NULL
+      )
+      ''');
+    }
   }
 
   // Close the database (optional, often not strictly necessary for app lifecycle)
