f88b450e1d9193512a048c9967a1987b64019a91
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-06-05 14:54:26 +0500

    Method for viewing

--- lib/Model/log_repository.dart
+++ lib/Model/log_repository.dart
@@ -2,7 +2,7 @@ import 'database_helper.dart';
 import 'package:sqflite/sqflite.dart';
 import 'log.dart';
 
-class LogRepository{
+class LogRepository {
   final DatabaseHelper _databaseHelper = DatabaseHelper();
 
   Future<int> insertLog(Log log) async {
@@ -14,6 +14,24 @@ class LogRepository{
     );
   }
 
+  Future<List<Log>> getAllLogs() async {
+    final db = await _databaseHelper.database;
+    final List<Map<String, dynamic>> maps = await db.query('logs');
 
-
+    return List.generate(maps.length, (i) {
+      return Log(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        detail: maps[i]['detail'],
+        purpose: maps[i]['purpose'],
+        date: maps[i]['date'],
+        timeFrom: maps[i]['timeFrom'],
+        timeTo: maps[i]['timeTo'],
+        remarks: maps[i]['remarks'],
+        initialMeterReading: maps[i]['initialMeterReading'],
+        finalMeterReading: maps[i]['finalMeterReading'],
+        kilometersCovered: maps[i]['kilometersCovered'],
+      );
+    });
+  }
 }
